1e4f119a8a80b2f0fabc5dc99040ea9d38c4a88e
jason20004 <jobexpel@gmail.com>
2025-09-03 11:46:21 +0100

    completed the basic functionality

---
+++ main.js
@@ -0,0 +1,12 @@
+console.log("testing if index.html points to this javascript file");
+
+let Whole_List = document.getElementById("todolist");
+let User_Input = document.getElementById("userInput");
+function addtheitem(){
+
+    let li_item = document.createElement("li");
+    li_item.textContent = User_Input.value;
+    Whole_List.appendChild(li_item);
+
+
+}
