479edcf5eb10f6f6023fc714d8e076385db9a021
jason20004 <jobexpel@gmail.com>
2025-09-03 13:01:06 +0100

    added delete feature

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ let User_Input = document.getElementById("userInput");
 function addtheitem(){
 
     let li_item = document.createElement("li");
-    li_item.textContent = User_Input.value;
+    let span_item = document.createElement("span");
+    span_item.textContent = " X";
+    
+    span_item.addEventListener("click",function(){
+
+        li_item.style.display = "none";
+    });
+
+    li_item.textContent = User_Input.value+"  ";
+
+    li_item.appendChild(span_item);
     Whole_List.appendChild(li_item);
 
         
