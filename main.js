4d92f592bc363c5c99bbbfda1d4159ada1e4d970
jasonMokuaProject <160226116+jasonMokuaProject@users.noreply.github.com>
2025-09-03 12:09:36 +0100

    Update main.js
    added a comment

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ console.log("testing if index.html points to this javascript file");
 
 let Whole_List = document.getElementById("todolist");
 let User_Input = document.getElementById("userInput");
+// function adds item 
 function addtheitem(){
 
     let li_item = document.createElement("li");
